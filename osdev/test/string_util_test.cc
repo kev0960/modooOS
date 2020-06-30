08818effae30af7131fc4a79202e443101cd2a16
Jaebum Lee <kev0960@gmail.com>
2020-06-29 23:18:52 -0700

    Delete unused folders (fs and test) at the top directory.

--- osdev/test/string_util_test.cc
+++
@@ -1,7 +0,0 @@
-#include "../boot/string_util.h"
-#include "test.h"
-
-TEST(StringUtilTest, StrLen) {
-  EXPECT_EQ(Kernel::strlen(""), 0);
-  EXPECT_EQ(Kernel::strlen("some string"), 11);
-}
--- osdev/test/syscall.h
+++
@@ -1,2 +0,0 @@
-extern "C" void *_syscall5(void *number, void *arg1, void *arg2, void *arg3,
-                           void *arg4, void *arg5);
